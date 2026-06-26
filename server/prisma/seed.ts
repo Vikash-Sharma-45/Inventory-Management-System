1b5a77c628c981a26e791fbccf2619cc5c2a5661
Vikash-Sharma-45 <vikashsharmavk58@gmail.com>
2026-06-26 16:50:30 +0530

    Seed Data Execution successfully & all errors are fixed

--- server/prisma/seed.ts
+++ server/prisma/seed.ts
@@ -1,7 +1,15 @@
+import "dotenv/config";
 import { PrismaClient } from "@prisma/client";
 import fs from "fs";
 import path from "path";
+import { fileURLToPath } from "url";
+
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = path.dirname(__filename);
+
 const prisma = new PrismaClient();
+
+
 
 async function deleteAllData(orderedFileNames: string[]) {
   const modelNames = orderedFileNames.map((fileName) => {
@@ -62,7 +70,8 @@ async function main() {
 
 main()
   .catch((e) => {
-    console.error(e);
+    console.error("Error during seeding:", e);
+    process.exit(1);
   })
   .finally(async () => {
     await prisma.$disconnect();
